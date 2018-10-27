f02027346f73486108d50a3ee12d872cd4bbf650
Craig Wendel <wendelcraig@gmail.com>
2018-10-27 12:55:17 -0500

    Add base files for home and sidebar, add scss files, base styling for app and index.

---
+++ src/components/Main.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../styles/main.scss'
+
+const Main = () => {
+    return (
+        <div>
+            <h2>This is the main screen</h2>
+        </div>
+    );
+};
+
+export default Main
---
+++ src/components/Sidebar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../styles/sidebar.scss'
+
+const Sidebar = () => {
+    return (
+        <div>
+            <h2>This is the Sidebar</h2>
+        </div>
+    );
+};
+
+export default Sidebar
